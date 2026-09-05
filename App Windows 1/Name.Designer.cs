163643ae6095cf7d13bc0379f7c7b61be530c86a
Willy Phelan <phelanguille@gmail.com>
2026-09-05 18:21:12 -0300

    Fromulario Envio de datos

---
+++ App Windows 1/Name.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace App_Windows_1
+{
+    partial class Name
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PrimerBoton = new System.Windows.Forms.Button();
+            this.lblEtiqueta = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lbldireccion = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // PrimerBoton
+            // 
+            this.PrimerBoton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.PrimerBoton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.PrimerBoton.Image = global::App_Windows_1.Properties.Resources.png;
+            this.PrimerBoton.Location = new System.Drawing.Point(78, 214);
+            this.PrimerBoton.Name = "PrimerBoton";
+            this.PrimerBoton.Size = new System.Drawing.Size(255, 23);
+            this.PrimerBoton.TabIndex = 0;
+            this.PrimerBoton.Text = "Haga Click";
+            this.PrimerBoton.UseVisualStyleBackColor = false;
+            this.PrimerBoton.Click += new System.EventHandler(this.PrimerBoton_Click);
+            // 
+            // lblEtiqueta
+            // 
+            this.lblEtiqueta.AutoSize = true;
+            this.lblEtiqueta.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEtiqueta.Location = new System.Drawing.Point(78, 102);
+            this.lblEtiqueta.Name = "lblEtiqueta";
+            this.lblEtiqueta.Size = new System.Drawing.Size(52, 15);
+            this.lblEtiqueta.TabIndex = 1;
+            this.lblEtiqueta.Text = "Nombre: ";
+            this.lblEtiqueta.Click += new System.EventHandler(this.lblEtiqueta_Click);
+            this.lblEtiqueta.MouseLeave += new System.EventHandler(this.lblEtiqueta_Click);
+      
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Location = new System.Drawing.Point(136, 102);
+            this.textBox1.MaxLength = 40;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox2.Location = new System.Drawing.Point(136, 128);
+            this.textBox2.MaxLength = 40;
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox2.Size = new System.Drawing.Size(197, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblApellido.Location = new System.Drawing.Point(78, 128);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(49, 15);
+            this.lblApellido.TabIndex = 3;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox3.Location = new System.Drawing.Point(136, 180);
+            this.textBox3.MaxLength = 40;
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox3.Size = new System.Drawing.Size(197, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // lbldireccion
+            // 
+            this.lbldireccion.AutoSize = true;
+            this.lbldireccion.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbldireccion.Location = new System.Drawing.Point(78, 180);
+            this.lbldireccion.Name = "lbldireccion";
+            this.lbldireccion.Size = new System.Drawing.Size(57, 15);
+            this.lbldireccion.TabIndex = 7;
+            this.lbldireccion.Text = "Direccion:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox4.Location = new System.Drawing.Point(136, 154);
+            this.textBox4.MaxLength = 40;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(197, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEdad.Location = new System.Drawing.Point(78, 154);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(37, 15);
+            this.lblEdad.TabIndex = 5;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(78, 254);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(250, 100);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // Name
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lbldireccion);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblEtiqueta);
+            this.Controls.Add(this.PrimerBoton);
+            this.Name = "Name";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Name";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Name_FormClosed);
+            this.Load += new System.EventHandler(this.Name_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PrimerBoton;
+        private System.Windows.Forms.Label lblEtiqueta;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lbldireccion;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ App Windows 1/Name.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_Windows_1 {
+    public partial class Name : Form {
+        public Name() {
+            
+        InitializeComponent() ; 
+        
+        this.MaximizeBox = false ;
+        this.MinimizeBox = false ;
+        
+        }
+
+   /*     private void Name_Load(object sender, EventArgs e){
+
+            MessageBox.Show("Bienvenidos, damas y caballeros,  a C#") ; 
+      
+            } */
+
+        private void Name_FormClosed(object sender, FormClosedEventArgs e){
+
+            MessageBox.Show("Gracias por usar la aplicación, hasta pronto") ;
+
+        }
+
+ /*  private void PrimerBoton_Click(object sender, EventArgs e)
+{
+    // string.IsNullOrWhiteSpace verifica tanto texto vacío como espacios en blanco
+    if (!string.IsNullOrWhiteSpace(textBox1.Text))
+    {
+        // Restablecemos el color normal de fondo (SystemColor)
+        textBox1.BackColor = SystemColors.Window; 
+
+        MessageBox.Show("El nombre ingresado es: " + textBox1.Text);
+    }
+    else
+    {
+        textBox1.BackColor = Color.Red;
+        MessageBox.Show("Por favor, ingrese un nombre.");
+    }
+}*/
+
+private void PrimerBoton_Click(object sender, EventArgs e)
+{
+    // Validación básica de campos vacíos
+    if (string.IsNullOrWhiteSpace(textBox1.Text) || 
+        string.IsNullOrWhiteSpace(textBox2.Text) || 
+        string.IsNullOrWhiteSpace(textBox4.Text) || 
+        string.IsNullOrWhiteSpace(textBox3.Text))
+    {
+        MessageBox.Show("Por favor, complete todos los campos antes de continuar.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        return;
+    }
+
+    // Captura de variables
+    string nombre = textBox1.Text;
+    string apellido = textBox2.Text;
+    string edad = textBox4.Text;
+    string direccion = textBox3.Text;
+
+    // Formateo e inyección del resultado en la caja multilínea
+    textBox5.Text = $"Nombre: {nombre}" + Environment.NewLine +
+                    $"Apellido: {apellido}" + Environment.NewLine +
+                    $"Edad: {edad}" + Environment.NewLine +
+                    $"Dirección: {direccion}";
+}
+
+
+     
+/*private void Name_MouseClick(object sender, MouseEventArgs e)
+{
+    // 'e' ya es MouseEventArgs, no hace falta castear nada
+    if (e.Button == MouseButtons.Left)
+        MessageBox.Show("Presionó el botón Izquierdo", "Atención");
+    else if (e.Button == MouseButtons.Right)
+        MessageBox.Show("Presionó el Botón Derecho", "Atención");
+    else if (e.Button == MouseButtons.Middle)
+        MessageBox.Show("Presionó el botón del Medio", "Atención");
+}
+*/
+        private void Name_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblEtiqueta_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void lblEtiqueta_MouseLeave(object sender, EventArgs e)
+        {
+            lblEtiqueta.BackColor = Color.Transparent ;
+            lblEtiqueta.Cursor = Cursors.Default ; 
+        }
+
+     /*   private void lblEtiqueta_MouseMove(object sender, MouseEventArgs e) {
+            
+            lblEtiqueta.BackColor = Color.Red ;
+            lblEtiqueta.Cursor = Cursors.Hand ; 
+        } 
+        */
+        private void textBox2_Leave(object sender, EventArgs e){ 
+        
+        MessageBox.Show("El apellido tiene: " + textBox2.Text.Length + " caracteres.") ;
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
